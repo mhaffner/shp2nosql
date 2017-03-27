c142218aed85464f2713e4d48257c181015276ad
mhaffner <matt.haffner@okstate.edu>
2017-03-27 14:01:41 -0500

    Update test script

--- test.sh
+++ test.sh
@@ -1,5 +1,16 @@
-echo "This script has basename `basename $0`, dirname `dirname $0`"
-echo "The present working directory is `pwd`"
+host=localhost
+port=9200
+index_name=dc_tracts
 
-my_dir=`dirname $0`
-echo $my_dir
+index_exists="$(curl -I "$host":"$port"/"$index_name")"
+if [[ "$index_exists" =~ .*"200"* ]]
+then
+    echo "Removing index $index_name"
+    curl -XDELETE "$host":"$port"/"$index_name"
+elif [[ "$index_exists" =~ .*"404"* ]]
+then
+    echo "Index does not exist; nothing to remove"
+else
+    echo "Connection refused. Is elasticsearch running?" >&2
+    exit 1
+fi
