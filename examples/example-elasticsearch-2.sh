8354ef41bd3e14faa28e874b9325e3879a430c62
Matthew Haffner <matt.haffner@okstate.edu>
2017-03-30 00:31:51 -0500

    Correct comment notation

--- examples/example-elasticsearch-2.sh
+++ examples/example-elasticsearch-2.sh
@@ -1,5 +1,5 @@
 shp2nosql \
-    -R `# remove the database if it exists` \
+    -R `# remove the index if it exists` \
     -d elasticsearch `# database type` \
     -f tract `# file to get from US Census TIGER files` \
     -S 25 `# state fips code (Massachusetts)` \
