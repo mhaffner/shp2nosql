375e443ca6f6f4b5b97db2fa5a527bd27532c5be
mhaffner <haffner.matthew.m@gmail.com>
2017-04-01 19:35:20 -0500

    Modify and make more robust

--- development/dependency-installation.sh
+++ development/dependency-installation.sh
@@ -3,11 +3,23 @@ sudo add-apt-repository universe # for gdal
 sudo add-apt-repository ppa:webupd8team/java # for java
 sudo apt-get update
 sudo apt-get install git gdal-bin curl oracle-java8-installer vim
+cd ~
+git clone https://github.com/mhaffner/shp2nosql
+PATH=$PATH:~/shp2nosql
 
 # install and start elasticsearch
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
 sudo apt-get update && sudo apt-get install elasticsearch
-# edit /etc/elasticsearch.yml and uncomment cluster.name and node.name
+cp elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 sudo systemctl enable elasticsearch.service
+sudo systemctl start elasticsearch
+
+# install and start mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo cp ~/shp2nosql/development/mongodb.service /etc/systemd/system/mongodb.service
+sudo systemctl start mongodb
