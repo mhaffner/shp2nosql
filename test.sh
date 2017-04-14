ed79add5c8863529371e84da7305c67c5b73b563
Matthew Haffner <matt.haffner@okstate.edu>
2017-04-13 19:15:54 -0500

    Modify script_dir variable to get directory of the package

--- test.sh
+++ test.sh
@@ -1,16 +1,3 @@
-host=localhost
-port=9200
-index_name=dc_tracts
-
-index_exists="$(curl -I "$host":"$port"/"$index_name")"
-if [[ "$index_exists" =~ .*"200"* ]]
-then
-    echo "Removing index $index_name"
-    curl -XDELETE "$host":"$port"/"$index_name"
-elif [[ "$index_exists" =~ .*"404"* ]]
-then
-    echo "Index does not exist; nothing to remove"
-else
-    echo "Connection refused. Is elasticsearch running?" >&2
-    exit 1
-fi
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )"
+script_dir="$( cd "$( dirname `which shp2nosql` )" && cd ../ && pwd )"
+echo $script_dir
