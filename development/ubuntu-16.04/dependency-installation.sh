df419cf97eaf2aae4ba89e4d21cb50995d352011
mhaffner <matt.haffner@okstate.edu>
2017-04-23 01:27:19 -0500

    Add proper path to config file

--- development/ubuntu-16.04/dependency-installation.sh
+++ development/ubuntu-16.04/dependency-installation.sh
@@ -21,5 +21,5 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-cp ~/shp2nosql/development/mongodb.service /etc/systemd/system/mongodb.service
+cp ~/shp2nosql/development/ubuntu-16.04/mongodb.service /etc/systemd/system/mongodb.service
 sudo systemctl start mongodb
