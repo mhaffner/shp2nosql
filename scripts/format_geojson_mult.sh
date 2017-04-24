47eec7e94576ce0de57bdb93c8d0ef9f57dd823e
mhaffner <matt.haffner@okstate.edu>
2017-04-23 19:58:51 -0500

    Remove whitespace

--- scripts/format_geojson_mult.sh
+++ scripts/format_geojson_mult.sh
@@ -4,8 +4,8 @@ format_geojson_mult () {
         for i in "$shapefile_dir"/*shp
         do
             ## need to get the name of each geojson based on the shapefile names
-            geojson="$(basename "$i" .shp).geojson" 
-            format_geojson $geojson # 
+            geojson="$(basename "$i" .shp).geojson"
+            format_geojson $geojson
         done
     else
         format_geojson "$geojson"
