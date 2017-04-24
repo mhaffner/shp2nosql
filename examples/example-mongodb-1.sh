6179a7c67868cfaf09630bc4dab3f76201e15ba2
mhaffner <matt.haffner@okstate.edu>
2017-04-23 20:12:03 -0500

    Change command name to shp2mongo, change arguments

--- examples/example-mongodb-1.sh
+++ examples/example-mongodb-1.sh
@@ -1,6 +1,5 @@
-shp2nosql \
+shp2mongo \
     -r `# remove the database if it exists` \
-    -d mongodb `# database type` \
     -f state `# file to get from US Census TIGER files` \
-    -D us_states `# database name` \
-    -c state `# collection`
+    -d us_states `# database name` \
+    -c state `# collection` \
