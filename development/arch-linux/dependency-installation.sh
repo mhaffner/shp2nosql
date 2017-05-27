5f0961034d91958482f27a906a043c5557db0b6e
Matthew Haffner <matt.haffner@okstate.edu>
2017-05-27 01:43:47 -0500

    Update dependencies

--- development/arch-linux/dependency-installation.sh
+++ development/arch-linux/dependency-installation.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syu git elasticsearch mongodb
+sudo pacman -Syu git elasticsearch mongodb wget
 sudo systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch
 sudo systemctl start mongodb
