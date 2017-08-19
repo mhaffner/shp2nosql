f72387ec364f622f07ab33d0e3d61354d1f244eb
Matthew Haffner <matt.haffner@okstate.edu>
2017-08-19 00:12:27 -0500

    Update Docker stuff

--- development/startup.sh
+++ development/startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# start elasticsearch and mongodb
+echo "Preparing Elasticsearch and MongoDB..."
 ./elasticsearch-5.5.2/bin/elasticsearch & disown
+echo "Elasticsearch started"
 ./mongodb-linux-x86_64-3.4.7/bin/mongod & disown
+echo "MongoDB started"
